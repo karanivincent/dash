5dfc5df0d4bf12292d25b4ab25ef40a7f8d3b97e
karanivincent <trevorvinnie@gmail.com>
2020-12-21 22:38:10 +0300

    can copy to clipboard

--- pages/index.vue
+++ pages/index.vue
@@ -698,13 +698,36 @@ export default {
                 let elem = this.$refs[`textarea-${index}`][0]
                 selectedText += elem.value + '\n'
               }
+              this.copyToClipboard(selectedText)
               console.log(selectedText)
             }
             // copy selection
           }
       }
     },
-
+    copyToClipboard(text) {
+      if (window.clipboardData && window.clipboardData.setData) {
+        // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+        return window.clipboardData.setData('Text', text)
+      } else if (
+        document.queryCommandSupported &&
+        document.queryCommandSupported('copy')
+      ) {
+        var textarea = document.createElement('textarea')
+        textarea.textContent = text
+        textarea.style.position = 'fixed' // Prevent scrolling to bottom of page in Microsoft Edge.
+        document.body.appendChild(textarea)
+        textarea.select()
+        try {
+          return document.execCommand('copy') // Security exception may be thrown by some browsers.
+        } catch (ex) {
+          console.warn('Copy to clipboard failed.', ex)
+          return false
+        } finally {
+          document.body.removeChild(textarea)
+        }
+      }
+    },
     insertCaption(index, text, time) {
       var roundedTime = Math.round((time + Number.EPSILON) * 100) / 100
 
