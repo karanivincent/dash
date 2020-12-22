07fefc8e608b3410e090d66feb9e590f122be200
karanivincent <trevorvinnie@gmail.com>
2020-12-22 19:57:42 +0300

    added volume and speed keybinds

--- pages/index.vue
+++ pages/index.vue
@@ -140,18 +140,34 @@ div.test {
               class="relative flex flex-col items-center px-1 py-1 rounded-md bg-gray-600 text-white text-2xl font-semibold focus:outline-none"
               @click="view.volumeSlider = !view.volumeSlider"
             >
-              <svg
-                class="w-8 h-5 hover:text-blue-500"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
+              <div v-if="videoMeta.volume == 0">
+                <svg
+                  class="w-8 h-5 hover:text-blue-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM12.293 7.293a1 1 0 011.414 0L15 8.586l1.293-1.293a1 1 0 111.414 1.414L16.414 10l1.293 1.293a1 1 0 01-1.414 1.414L15 11.414l-1.293 1.293a1 1 0 01-1.414-1.414L13.586 10l-1.293-1.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <div v-else>
+                <svg
+                  class="w-8 h-5 hover:text-blue-500"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M9.383 3.076A1 1 0 0110 4v12a1 1 0 01-1.707.707L4.586 13H2a1 1 0 01-1-1V8a1 1 0 011-1h2.586l3.707-3.707a1 1 0 011.09-.217zM14.657 2.929a1 1 0 011.414 0A9.972 9.972 0 0119 10a9.972 9.972 0 01-2.929 7.071 1 1 0 01-1.414-1.414A7.971 7.971 0 0017 10c0-2.21-.894-4.208-2.343-5.657a1 1 0 010-1.414zm-2.829 2.828a1 1 0 011.415 0A5.983 5.983 0 0115 10a5.984 5.984 0 01-1.757 4.243 1 1 0 01-1.415-1.415A3.984 3.984 0 0013 10a3.983 3.983 0 00-1.172-2.828 1 1 0 010-1.415z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
               <div class="h-4 text-xs font-thin">{{ videoMeta.volume }}%</div>
               <div
                 class="absolute rounded-md overflow-hidden text-sm text-gray-800 bg-gray-100 w-48 h-32 mt-10 z-10"
@@ -732,6 +748,34 @@ export default {
             this.copyToClipboard(selectedText)
             // copy selection
           }
+          break
+        case 190:
+          // Period
+          if (event.ctrlKey) {
+            // Add video play speed
+            this.setPlaybackspeed('fast')
+          }
+          break
+        case 188:
+          // Comma
+          if (event.ctrlKey) {
+            // Reduce video play speed
+            this.setPlaybackspeed('slow')
+          }
+          break
+        case 219:
+          // Comma
+          if (event.ctrlKey) {
+            // Reduce video play speed
+            this.reduceVolume()
+          }
+          break
+        case 221:
+          // Comma
+          if (event.ctrlKey) {
+            // Reduce video play speed
+            this.addVolume()
+          }
       }
     },
     firstLettercaps(i) {
@@ -870,8 +914,27 @@ export default {
 
       this.$refs.videoMeta.volume = this.videoMeta.volume / 100
     },
-    setPlaybackspeed() {
-      this.$refs.videoMeta.playbackRate = this.videoMeta.playbackspeed / 100
+    setPlaybackspeed(speed = 0) {
+      if (speed == 0)
+        this.$refs.videoMeta.playbackRate = this.videoMeta.playbackspeed / 100
+      else {
+        switch (speed) {
+          case 'fast':
+            if (this.videoMeta.playbackspeed < 200) {
+              this.videoMeta.playbackspeed += 10
+              this.$refs.videoMeta.playbackRate =
+                this.videoMeta.playbackspeed / 100
+            }
+            break
+          case 'slow':
+            if (this.videoMeta.playbackspeed > 0) {
+              this.videoMeta.playbackspeed -= 10
+              this.$refs.videoMeta.playbackRate =
+                this.videoMeta.playbackspeed / 100
+            }
+            break
+        }
+      }
     },
   },
 }
